272ee61b67448f5f36b0ea80b891c2d78b943353
pengxianhong <290677893@qq.com>
2019-07-22 10:37:30 +0800

    补充上一次修改 7/22

--- app/src/main/java/com/example/mutidemo/ui/AutoCreateFragmentActivity.java
+++ app/src/main/java/com/example/mutidemo/ui/AutoCreateFragmentActivity.java
@@ -3,12 +3,19 @@ package com.example.mutidemo.ui;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mutidemo.R;
 import com.example.mutidemo.ui.fragment.TestFragment;
 import com.pengxh.app.multilib.base.BaseNormalActivity;
+import com.pengxh.app.multilib.utils.ToastUtil;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -22,6 +29,13 @@ public class AutoCreateFragmentActivity extends BaseNormalActivity {
     TabLayout mTabLayout;
     @BindView(R.id.mViewPager)
     ViewPager mViewPager;
+    @BindView(R.id.mBtnAdd)
+    Button mBtnAdd;
+    @BindView(R.id.mBtnDel)
+    Button mBtnDel;
+
+    private LinkedList<String> stringList = new LinkedList<>();//链表更适合增删操作
+    private FragmentPagerAdapter pagerAdapter;
 
     @Override
     public void initView() {
@@ -35,32 +49,56 @@ public class AutoCreateFragmentActivity extends BaseNormalActivity {
 
     @Override
     public void initEvent() {
-        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
-
-            // titles作为滑动标题栏，动态添加数据进去就可以了
-            String[] titles = {"全部", "待付款", "待发货", "待收货", "待评价"};
-
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public Fragment getItem(int index) {//动态创建fragment
-                TestFragment fragment = new TestFragment();
-                Bundle bundle = new Bundle();
-                bundle.putInt("position", index);
-                bundle.putString("title", titles[index]);
-                fragment.setArguments(bundle);
-                return fragment;
+            public void onClick(View v) {
+                stringList.addLast("第一个页面");
+                pagerAdapter.notifyDataSetChanged();//哪个地方改变了数据，那个地方就需要调用
             }
-
+        });
+        mBtnDel.setOnClickListener(new View.OnClickListener() {
             @Override
-            public int getCount() {// 添加fragment个数
-                return titles.length;
+            public void onClick(View v) {
+                if (stringList.size() <= 0) {
+                    ToastUtil.showBeautifulToast("Fuck! 再点就炸了", ToastUtil.WARNING);
+                } else {
+                    stringList.removeLast();
+                    pagerAdapter.notifyDataSetChanged();
+                }
             }
-
-            @Override
-            public CharSequence getPageTitle(int position) { // 添加fragment标题
-                return titles[position];
-            }
-        };
-        mViewPager.setAdapter(adapter);
+        });
+        pagerAdapter = new BaseFragmentAdapter(getSupportFragmentManager(), stringList);
+        mViewPager.setAdapter(pagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private class BaseFragmentAdapter extends FragmentPagerAdapter {
+
+        private List<String> titleList;
+
+        BaseFragmentAdapter(FragmentManager fm, List<String> titleList) {
+            super(fm);
+            this.titleList = titleList;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            TestFragment fragment = new TestFragment();
+            Bundle bundle = new Bundle();
+            bundle.putInt("position", position);
+            bundle.putString("title", titleList.get(position));
+            fragment.setArguments(bundle);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return titleList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) { // 添加fragment标题
+            return titleList.get(position);
+        }
     }
 }
