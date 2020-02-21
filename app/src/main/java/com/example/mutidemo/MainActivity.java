d427e641b89ec0efb0efe3d702a39e9821b215d9
pengxianhong <290677893@qq.com>
2020-02-21 18:49:53 +0800

    添加扫一扫，基于ZBar

--- app/src/main/java/com/example/mutidemo/MainActivity.java
+++ app/src/main/java/com/example/mutidemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mutidemo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 
@@ -22,6 +23,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
+import cn.bertsir.zbar.Qr.ScanResult;
+import cn.bertsir.zbar.QrConfig;
+import cn.bertsir.zbar.QrManager;
+import cn.bertsir.zbar.view.ScanLineView;
 
 public class MainActivity extends DoubleClickExitActivity {
 
@@ -30,7 +35,7 @@ public class MainActivity extends DoubleClickExitActivity {
 
     private Context mContext = MainActivity.this;
     private List<String> mItemNameList = Arrays.asList("SharedPreferences", "BMOB_SDK登陆注册"
-            , "仿iOS风格对话框", "MVP网络请求框架", "BottomNavigationView");
+            , "仿iOS风格对话框", "MVP网络请求框架", "BottomNavigationView", "ZBar扫一扫");
     private BroadcastManager broadcastManager;
 
     @Override
@@ -68,23 +73,70 @@ public class MainActivity extends DoubleClickExitActivity {
             switch (position) {
                 case 0:
                     intent.setClass(mContext, SharedPreferencesActivity.class);
+                    startActivity(intent);
                     break;
                 case 1:
                     intent.setClass(mContext, UserManagerActivity.class);
+                    startActivity(intent);
                     break;
                 case 2:
                     intent.setClass(mContext, BottomDialogActivity.class);
+                    startActivity(intent);
                     break;
                 case 3:
                     intent.setClass(mContext, MVPActivity.class);
+                    startActivity(intent);
                     break;
                 case 4:
                     intent.setClass(mContext, BottomNavigationActivity.class);
+                    startActivity(intent);
+                    break;
+                case 5:
+                    //开始扫一扫
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            startScannerActivity();
+                        }
+                    }).start();
                     break;
                 default:
                     break;
             }
-            startActivity(intent);
+        });
+    }
+
+    private void startScannerActivity() {
+        QrConfig qrConfig = new QrConfig.Builder()
+                .setTitleText("扫一扫")//设置Tilte文字
+                .setShowLight(true)//显示手电筒按钮
+                .setShowTitle(true)//显示Title
+                .setShowAlbum(true)//显示从相册选择按钮
+                .setCornerColor(Color.parseColor("#0094FF"))//设置扫描框颜色
+                .setLineColor(Color.parseColor("#0094FF"))//设置扫描线颜色
+                .setLineSpeed(QrConfig.LINE_MEDIUM)//设置扫描线速度
+                .setDesText(null)//扫描框下文字
+                .setShowDes(true)//是否显示扫描框下面文字
+                .setPlaySound(true)//是否扫描成功后bi~的声音
+                .setDingPath(R.raw.qrcode)//设置提示音(不设置为默认的Ding~)
+                .setIsOnlyCenter(true)//是否只识别框中内容(默认为全屏识别)
+                .setTitleBackgroudColor(Color.parseColor("#262020"))//设置状态栏颜色
+                .setTitleTextColor(Color.WHITE)//设置Title文字颜色
+                .setScreenOrientation(QrConfig.SCREEN_PORTRAIT)//设置屏幕方式
+                .setOpenAlbumText("选择要识别的图片")//打开相册的文字
+                .setScanLineStyle(ScanLineView.style_hybrid)//扫描线样式
+                .setShowVibrator(true)//是否震动提醒
+                .create();
+        QrManager.getInstance().init(qrConfig).startScan(this, new QrManager.OnScanResultCallback() {
+            @Override
+            public void onScanSuccess(ScanResult result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        EasyToast.showToast("扫码结果: " + result.content, EasyToast.SUCCESS);
+                    }
+                });
+            }
         });
     }
 
--- app/src/main/java/com/example/mutidemo/WelcomeActivity.java
+++ app/src/main/java/com/example/mutidemo/WelcomeActivity.java
@@ -27,7 +27,7 @@ public class WelcomeActivity extends AppCompatActivity implements EasyPermission
 
     private static final int PERMISSIONS_CODE = 999;
     private static final String[] USER_PERMISSIONS = {Manifest.permission.READ_EXTERNAL_STORAGE
-            , Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            , Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
