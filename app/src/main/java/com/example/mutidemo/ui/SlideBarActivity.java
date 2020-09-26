83eb871611330470a9a9b5e7de6037988083b820
pengxianhong <290677893@qq.com>
2020-09-26 21:33:02 +0800

    添加联系人侧滑列表控件-2

--- app/src/main/java/com/example/mutidemo/ui/SlideBarActivity.java
+++ app/src/main/java/com/example/mutidemo/ui/SlideBarActivity.java
@@ -1,9 +1,42 @@
 package com.example.mutidemo.ui;
 
+import android.content.Context;
+import android.os.CountDownTimer;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.mutidemo.R;
+import com.example.mutidemo.widget.SlideBarView;
 import com.pengxh.app.multilib.base.BaseNormalActivity;
+import com.pengxh.app.multilib.widget.EasyToast;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+import butterknife.BindView;
 
 public class SlideBarActivity extends BaseNormalActivity {
+
+    private static final String TAG = "SlideBarActivity";
+    private static final String[] CITY = new String[]{"安国市", "霸州市", "保定市", "泊头市", "沧州市",
+            "承德市", "定州市", "高碑店市", "邯郸市", "河间市", "衡水市", "黄骅市", "冀州市", "晋州市",
+            "廊坊市", "南宫市", "迁安市", "秦皇岛市", "任丘市", "三河市", "沙河市", "深州市", "石家庄市",
+            "唐山市", "武安市", "辛集市", "新乐市", "邢台市", "张家口市", "遵化市", "涿州市"};
+    @BindView(R.id.cityRecyclerView)
+    RecyclerView cityRecyclerView;
+    @BindView(R.id.slideBarView)
+    SlideBarView slideBarView;
 
     @Override
     public int initLayoutView() {
@@ -12,11 +45,142 @@ public class SlideBarActivity extends BaseNormalActivity {
 
     @Override
     public void initData() {
-
+        CityAdapter cityAdapter = new CityAdapter(this, CITY);
+        cityRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        cityRecyclerView.setAdapter(cityAdapter);
+        cityAdapter.setOnCityItemClickListener(new CityAdapter.OnCityItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                EasyToast.showToast(CITY[position], EasyToast.DEFAULT);
+            }
+        });
     }
 
     @Override
     public void initEvent() {
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        View rootView = layoutInflater.inflate(R.layout.activity_slide, null);
+        View contentView = layoutInflater.inflate(R.layout.layout_popup, null);
+        PopupWindow popupWindow = new PopupWindow(contentView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
+        popupWindow.setContentView(contentView);
+        TextView letterView = contentView.findViewById(R.id.letterView);
+        CountDownTimer countDownTimer = new CountDownTimer(1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
 
+            }
+
+            @Override
+            public void onFinish() {
+                popupWindow.dismiss();
+            }
+        };
+        slideBarView.setOnIndexChangeListener(new SlideBarView.OnIndexChangeListener() {
+            @Override
+            public void OnIndexChange(String letter) {
+                //在屏幕中间放大显示被按到的字母
+                letterView.setText(letter);
+                popupWindow.showAtLocation(rootView, Gravity.CENTER, 0, 0);
+                countDownTimer.start();
+
+                //根据滑动显示的字母索引到城市名字第一个汉字
+                cityRecyclerView.smoothScrollToPosition(obtainFirstLetterIndex(letter));
+            }
+        });
+    }
+
+    private int obtainFirstLetterIndex(String letter) {
+        int index = 0;
+        for (int i = 0; i < CITY.length; i++) {
+            String firstWord = CITY[i].substring(0, 1);
+            //转拼音
+            String firstLetter = getFirstLetter(firstWord);
+            if (letter.equals(firstLetter)) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static String getFirstLetter(String chinese) {
+        StringBuilder pinyinStr = new StringBuilder();
+        char[] newChar = chinese.toCharArray();  //转为单个字符
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (char c : newChar) {
+            if (c > 128) {
+                try {
+                    pinyinStr.append(PinyinHelper.toHanyuPinyinStringArray(c, defaultFormat)[0].charAt(0));
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            } else {
+                pinyinStr.append(c);
+            }
+        }
+        return pinyinStr.toString();
+    }
+
+    static class CityAdapter extends RecyclerView.Adapter<CityAdapter.ViewHolder> {
+
+        private LayoutInflater inflater;
+        private String[] mCityItem;
+
+        CityAdapter(Context mContext, String[] array) {
+            this.mCityItem = array;
+            inflater = LayoutInflater.from(mContext);
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(inflater.inflate(R.layout.item_city_recyclerview, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.bindHolder(mCityItem[position]);
+            if (mOnItemClickListener != null) {
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mOnItemClickListener.onClick(position);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCityItem == null ? 0 : mCityItem.length;
+        }
+
+        static class ViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView cityNameTag;
+            private TextView cityName;
+
+            ViewHolder(@NonNull View itemView) {
+                super(itemView);
+                cityNameTag = itemView.findViewById(R.id.cityNameTag);
+                cityName = itemView.findViewById(R.id.cityName);
+            }
+
+            void bindHolder(String city) {
+                cityNameTag.setText(city.substring(0, 1));
+                cityName.setText(city);
+            }
+        }
+
+        private OnCityItemClickListener mOnItemClickListener;
+
+        public interface OnCityItemClickListener {
+            void onClick(int position);
+        }
+
+        void setOnCityItemClickListener(OnCityItemClickListener listener) {
+            this.mOnItemClickListener = listener;
+        }
     }
 }
--- app/src/main/java/com/example/mutidemo/widget/SlideBarView.java
+++ app/src/main/java/com/example/mutidemo/widget/SlideBarView.java
@@ -5,28 +5,34 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.example.mutidemo.R;
 
-public class SlideBarView extends View {
+public class SlideBarView extends View implements View.OnTouchListener {
 
     private static final String TAG = "SlideBarView";
-    private static final String[] LETTER = {"#", "A", "B", "C", "D", "E", "F", "G", "H",
-            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private static final String[] LETTER = {"A", "B", "C", "D", "E", "F", "G", "H",
+            "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "#"};
     private static final int viewWidth = 20;
     private Context mContext;
     private float centerX;//中心x
     private int textSize;
     private int textColor;
+    private int circleColor;
     private TextPaint textPaint;//文字画笔
     private int mHeight;//控件的实际尺寸
+    private int touchIndex = -1;
+    private int letterHeight;
+    private Paint paint;
 
     public SlideBarView(Context context) {
         this(context, null, 0);
@@ -44,49 +50,45 @@ public class SlideBarView extends View {
         textSize = a.getDimensionPixelOffset(R.styleable.SlideBarView_slide_textSize
                 , sp2px(context, 18));
         textColor = a.getColor(R.styleable.SlideBarView_slide_textColor, Color.LTGRAY);
+        circleColor = a.getColor(R.styleable.SlideBarView_slide_circleColor, Color.parseColor("#43DB87"));
         a.recycle();
 
         //初始化画笔
         initPaint();
+        //触摸事件
+        setOnTouchListener(this);
     }
 
     private void initPaint() {
+        //文字画笔
         textPaint = new TextPaint();
         textPaint.setAntiAlias(true);
         textPaint.setTextSize(textSize);
         textPaint.setColor(textColor);
+
+        //被按到的字母底部背景
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(circleColor);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        //圆心位置
         centerX = w >> 1;
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
-        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
         int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
         // 获取宽
-        int mWidth;
-        if (widthSpecMode == MeasureSpec.EXACTLY) {
-            // match_parent/精确值
-            mWidth = widthSpecSize;
-        } else {
-            // wrap_content
-            mWidth = dp2px(mContext, viewWidth);
-        }
+        int mWidth = dp2px(mContext, viewWidth);
         // 获取高
         if (heightSpecMode == MeasureSpec.EXACTLY) {
             // match_parent/精确值
             mHeight = heightSpecSize;
-        } else {
-            // wrap_content
-            mHeight = dp2px(mContext, viewWidth);
         }
         // 设置该view的宽高
         setMeasuredDimension(mWidth, mHeight);
@@ -96,14 +98,19 @@ public class SlideBarView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        //计算每个字母的坐标
-        int letterHeight = mHeight / LETTER.length;
-        /**
-         * 每个字母的X坐标=屏幕宽度-(mWidth/2)
-         * 每个字母的Y坐标=i*letterHeight+(letterHeight/2)
-         * */
+        letterHeight = mHeight / LETTER.length;
         for (int i = 0; i < LETTER.length; i++) {
-            int y = i * letterHeight + letterHeight >> 1;
+            int y = (i + 1) * letterHeight;//每个字母的占位高度(不是字体高度)
+
+            //字母变色
+            if (touchIndex == i) {
+                //让当前字母变色
+                textPaint.setColor(Color.WHITE);
+                canvas.drawCircle(centerX, (float) ((i + 0.4) * letterHeight), dp2px(mContext, 10.0f), paint);
+            } else {
+                //其他字母不变色
+                textPaint.setColor(textColor);
+            }
 
             //绘制文字
             String letter = LETTER[i];
@@ -113,10 +120,49 @@ public class SlideBarView extends View {
             int textHeight = textRect.height();
             //计算文字左下角坐标
             float textX = centerX - (textWidth >> 1);
-            float textY = y + (textHeight * 2);
+            float textY = y - (textHeight >> 1);
             canvas.drawText(letter, textX, textY, textPaint);
         }
     }
+
+    //侧边栏滑动事件
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                float y = Math.abs(event.getY());//取绝对值，不然y可能会取到负值
+                int index = (int) (y / letterHeight);//字母的索引
+                if (index != touchIndex) {
+                    touchIndex = Math.min(index, LETTER.length - 1);
+                    //点击设置中间字母
+                    if (onIndexChangeListener != null) {
+                        onIndexChangeListener.OnIndexChange(LETTER[touchIndex]);
+                    }
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                touchIndex = -1;
+                invalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private OnIndexChangeListener onIndexChangeListener;
+
+    public void setOnIndexChangeListener(OnIndexChangeListener listener) {
+        onIndexChangeListener = listener;
+    }
+
+    public interface OnIndexChangeListener {
+        void OnIndexChange(String letter);
+    }
+
 
     /**
      * sp转换成px
