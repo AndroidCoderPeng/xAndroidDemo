ecd11808382e74891449d965fcd6959983baadd1
Pengxh <290677893@qq.com>
2022-01-17 10:07:59 +0800

    完善CameraX预览

--- app/src/main/java/com/example/mutidemo/ui/FaceCollectionActivity.java
+++ app/src/main/java/com/example/mutidemo/ui/FaceCollectionActivity.java
@@ -3,11 +3,15 @@ package com.example.mutidemo.ui;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.Rect;
 import android.media.Image;
 import android.media.ImageReader;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
+import androidx.camera.core.AspectRatio;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
@@ -25,6 +29,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.nio.ByteBuffer;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -39,9 +44,12 @@ public class FaceCollectionActivity extends BaseNormalActivity {
     @BindView(R.id.faceCollectionView)
     FaceCollectionView faceCollectionView;
 
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ExecutorService cameraExecutor;
+    private WindowManager windowManager;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageCapture imageCapture;
+    private static final double RATIO_4_3_VALUE = 4.0 / 3.0;
+    private static final double RATIO_16_9_VALUE = 16.0 / 9.0;
 
     @Override
     public int initLayoutView() {
@@ -50,8 +58,12 @@ public class FaceCollectionActivity extends BaseNormalActivity {
 
     @Override
     public void initData() {
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        // Initialize WindowManager to retrieve display metrics
+        windowManager = getWindowManager();
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        //检查 CameraProvider 可用性
+        // 检查 CameraProvider 可用性
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
@@ -68,16 +80,60 @@ public class FaceCollectionActivity extends BaseNormalActivity {
     }
 
     private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
-        cameraExecutor = Executors.newSingleThreadExecutor();
-        Preview cameraPreview = new Preview.Builder().build();
-        imageCapture = new ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .build();
+        // Get screen metrics used to setup camera for full screen resolution
+        int screenAspectRatio;
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            Rect metrics = windowManager.getCurrentWindowMetrics().getBounds();
+            screenAspectRatio = aspectRatio(metrics.width(), metrics.height());
+        } else {
+            DisplayMetrics outMetrics = new DisplayMetrics();
+            windowManager.getDefaultDisplay().getMetrics(outMetrics);
+            screenAspectRatio = aspectRatio(outMetrics.widthPixels, outMetrics.heightPixels);
+        }
+
+        int rotation;
+        if (android.os.Build.VERSION.SDK_INT >= 30) {
+            rotation = Objects.requireNonNull(getDisplay()).getRotation();
+        } else {
+            rotation = windowManager.getDefaultDisplay().getRotation();
+        }
+
+        // CameraSelector
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
                 .build();
-        cameraPreview.setSurfaceProvider(cameraPreView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, cameraPreview);
+
+        // Preview
+        Preview cameraPreview = new Preview.Builder()
+                .setTargetAspectRatio(screenAspectRatio)
+                .setTargetRotation(rotation)
+                .build();
+
+        // ImageCapture
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setTargetAspectRatio(screenAspectRatio)
+                .setTargetRotation(rotation)
+                .build();
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll();
+        try {
+            cameraProvider.bindToLifecycle(this, cameraSelector, imageCapture, cameraPreview);
+
+            // Attach the viewfinder's surface provider to preview use case
+            cameraPreview.setSurfaceProvider(cameraPreView.getSurfaceProvider());
+        } catch (Exception e) {
+            Log.e(TAG, "Use case binding failed", e);
+        }
+    }
+
+    private int aspectRatio(int width, int height) {
+        double ratio = (double) Math.max(width, height) / Math.min(width, height);
+        if (Math.abs(ratio - RATIO_4_3_VALUE) <= Math.abs(ratio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3;
+        }
+        return AspectRatio.RATIO_16_9;
     }
 
     public void takePicture() {
