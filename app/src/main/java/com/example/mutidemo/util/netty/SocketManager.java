028da65eefce5bd1764f487330f224184386e270
pengxianhong <290677893@qq.com>
2022-11-28 00:33:00 +0800

    fix(*): 添加byte补位导致计算错误问题

--- app/src/main/java/com/example/mutidemo/util/netty/SocketManager.java
+++ app/src/main/java/com/example/mutidemo/util/netty/SocketManager.java
@@ -55,35 +55,51 @@ public class SocketManager implements ISocketListener {
          * 0xC1
          *
          * [-1, 1, 1, 55, -26, 0, 0, -78, 53, 10, 61, 5, 111, -63]
-         * [FF 01 01 37 E6 00 00 B2 35 0A 3D 05 6F C1]
+         * [FF  01 01 37  E6   00 00  B2  35  0A  3D  05 6F   C1]
          * 甲烷浓度值为79638，计算为79638=0x01*65536+0x37*256+0xE6[0x01为数据码1，0x37为数据码2，0xE6为数据码3]；
          * 激光甲烷设备状态值为0，表示状态正常，[0x00为数据码4]；
          * 激光强度值为45621，计算为45621=0x00*65536+0xB2*256+0x35[0x00为数据码5，0xB2为数据码6，0x35为数据码7];
          * */
+        int[] bytes = bytesToUnsigned(data);
         HashMap<String, Object> hashMap = new HashMap<>();
-        byte[] methaneBytes = new byte[3];
-        System.arraycopy(data, 2, methaneBytes, 0, 3);
+        int[] methaneBytes = new int[3];
+        System.arraycopy(bytes, 2, methaneBytes, 0, 3);
         hashMap.put("methane", covertDataValue(methaneBytes));
 
-        hashMap.put("methaneState", covertState(data[5]));
+        hashMap.put("methaneState", covertState(bytes[5]));
 
-        byte[] laserBytes = new byte[3];
-        System.arraycopy(data, 6, laserBytes, 0, 3);
+        int[] laserBytes = new int[3];
+        System.arraycopy(bytes, 6, laserBytes, 0, 3);
         hashMap.put("laser", covertDataValue(laserBytes));
 
-        byte[] horizontalBytes = new byte[2];
-        System.arraycopy(data, 9, horizontalBytes, 0, 2);
+        int[] horizontalBytes = new int[2];
+        System.arraycopy(bytes, 9, horizontalBytes, 0, 2);
         hashMap.put("horizontal", covertAngleValue(horizontalBytes));
 
-        byte[] verticalBytes = new byte[2];
-        System.arraycopy(data, 11, verticalBytes, 0, 2);
+        int[] verticalBytes = new int[2];
+        System.arraycopy(bytes, 11, verticalBytes, 0, 2);
         hashMap.put("vertical", covertAngleValue(verticalBytes));
 
         //{"horizontal":26.21,"laser":-19915,"methaneState":"正常","methane":79590,"vertical":13.91}
         Log.d(TAG, "onMessageResponse ===> " + new Gson().toJson(hashMap));
     }
 
-    private int covertDataValue(byte[] bytes) {
+    public static int[] bytesToUnsigned(byte[] data) {
+        int[] array = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            byte datum = data[i];
+            int temp;
+            if (datum < 0) {
+                temp = 0xFF & datum;
+            } else {
+                temp = datum;
+            }
+            array[i] = temp;
+        }
+        return array;
+    }
+
+    private int covertDataValue(int[] bytes) {
         //数据码1* 65536 + 数据码2 * 256 + 数据码3
         return bytes[0] * 65536 + bytes[1] * 256 + bytes[2];
     }
@@ -106,7 +122,7 @@ public class SocketManager implements ISocketListener {
         return state;
     }
 
-    private double covertAngleValue(byte[] bytes) {
+    private double covertAngleValue(int[] bytes) {
         //首先计算Tangle=[数据码10 * 256 + 数据码11]/100，单位为°，精确到0.01。
         //若Tangle在0~90范围内，则垂直角度值=Tangle；若Tangle在-1~-90范围内，则垂直角度值=Tangle-360)
         double tangle = (double) ((bytes[0] * 256) + bytes[1]) / 100;
