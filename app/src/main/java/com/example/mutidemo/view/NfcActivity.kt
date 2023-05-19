70d4e667959bf1787abb3fedca95b17b00af2459
pengxianhong <290677893@qq.com>
2023-05-19 23:45:42 +0800

    fix(*): 优化NFC逻辑

--- app/src/main/java/com/example/mutidemo/view/NfcActivity.kt
+++ app/src/main/java/com/example/mutidemo/view/NfcActivity.kt
@@ -5,28 +5,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.os.CountDownTimer
 import com.example.mutidemo.R
 import com.example.mutidemo.extensions.toHex
-import com.example.mutidemo.util.LoadingDialogHub
 import com.pengxh.kt.lite.base.KotlinBaseActivity
-import com.pengxh.kt.lite.extensions.show
 import kotlinx.android.synthetic.main.activity_nfc.*
 
 
 class NfcActivity : KotlinBaseActivity() {
 
     private val kTag = "NfcActivity"
-    private val timer: CountDownTimer = object : CountDownTimer(5000, 1000) {
-        override fun onFinish() {
-            LoadingDialogHub.dismiss()
-            "读卡失败，请重新贴卡读取".show(this@NfcActivity)
-        }
-
-        override fun onTick(millisUntilFinished: Long) {
-
-        }
-    }
+    private lateinit var filters: Array<IntentFilter>
+    private lateinit var techList: Array<Array<String>>
     private lateinit var nfcAdapter: NfcAdapter
     private lateinit var pIntent: PendingIntent
 
@@ -39,43 +28,39 @@ class NfcActivity : KotlinBaseActivity() {
     }
 
     override fun initEvent() {
-        readCardButton.setChangeAlphaWhenPress(true)
-        readCardButton.setOnClickListener {
-            LoadingDialogHub.show(this, "读卡中，请稍后...")
-            val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
-            val tag = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
-            val tech = IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED)
-            val filters = arrayOf(ndef, tag, tech)
-            val techList = arrayOf(
-                arrayOf(
-                    "android.nfc.tech.Ndef",
-                    "android.nfc.tech.NfcA",
-                    "android.nfc.tech.NfcB",
-                    "android.nfc.tech.NfcF",
-                    "android.nfc.tech.NfcV",
-                    "android.nfc.tech.NdefFormatable",
-                    "android.nfc.tech.MifareClassic",
-                    "android.nfc.tech.MifareUltralight",
-                    "android.nfc.tech.NfcBarcode"
-                )
+        val ndef = IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED)
+        val tag = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+        val tech = IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED)
+        filters = arrayOf(ndef, tag, tech)
+        techList = arrayOf(
+            arrayOf(
+                "android.nfc.tech.Ndef",
+                "android.nfc.tech.NfcA",
+                "android.nfc.tech.NfcB",
+                "android.nfc.tech.NfcF",
+                "android.nfc.tech.NfcV",
+                "android.nfc.tech.NdefFormatable",
+                "android.nfc.tech.MifareClassic",
+                "android.nfc.tech.MifareUltralight",
+                "android.nfc.tech.NfcBarcode"
             )
-            nfcAdapter.enableForegroundDispatch(this, pIntent, filters, techList)
-            timer.start()
-        }
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        nfcAdapter.enableForegroundDispatch(this, pIntent, filters, techList)
     }
 
     override fun onPause() {
         super.onPause()
         nfcAdapter.disableForegroundDispatch(this)
-        timer.cancel()
     }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         setIntent(intent)
         val tag = getIntent().getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-        LoadingDialogHub.dismiss()
-        timer.cancel()
         cardValueView.text = tag?.id?.toHex()
     }
 
