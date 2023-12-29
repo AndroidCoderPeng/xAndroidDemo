551bd2152094651536be0a513a410d9bbea420c4
Pengxh <290677893@qq.com>
2023-12-29 09:39:29 +0800

    perf(*): 优化SlideBarView，未完

--- app/src/main/java/com/example/multidemo/widget/SlideBarView.kt
+++ app/src/main/java/com/example/multidemo/widget/SlideBarView.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.graphics.RectF
+import android.graphics.Typeface
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.MotionEvent
@@ -33,15 +35,18 @@ class SlideBarView constructor(context: Context, attrs: AttributeSet) : View(con
 
     private var touchIndex = -1
     private var letterHeight = 0
-    private var showBackground = false
+    private var showBackground = true
 
+    private lateinit var tickPaint: Paint
+    private lateinit var ancherPaint: Paint
     private lateinit var viewBgRectF: RectF
-    private lateinit var textPaint: TextPaint
     private lateinit var backgroundPaint: Paint
+    private lateinit var textPaint: TextPaint
+    private lateinit var textRect: Rect
 
     init {
         val attr = context.obtainStyledAttributes(attrs, R.styleable.SlideBarView)
-        viewWidth = attr.getDimension(R.styleable.SlideBarView_slide_width, 25f)
+        viewWidth = attr.getDimension(R.styleable.SlideBarView_slide_width, 30f)
         roundRadius = viewWidth / 2f
         textSize = attr.getDimension(R.styleable.SlideBarView_slide_textSize, 18f)
         textColor = attr.getColor(R.styleable.SlideBarView_slide_textColor, Color.LTGRAY)
@@ -55,6 +60,16 @@ class SlideBarView constructor(context: Context, attrs: AttributeSet) : View(con
 
 
     private fun initPaint() {
+        tickPaint = Paint()
+        tickPaint.color = Color.DKGRAY
+        tickPaint.style = Paint.Style.STROKE
+        tickPaint.strokeWidth = 1f
+        tickPaint.isAntiAlias = true
+
+        ancherPaint = Paint()
+        ancherPaint.color = Color.RED
+        ancherPaint.isAntiAlias = true
+
         //背景色画笔
         backgroundPaint = Paint()
         backgroundPaint.color = Color.LTGRAY
@@ -65,6 +80,8 @@ class SlideBarView constructor(context: Context, attrs: AttributeSet) : View(con
         textPaint.isAntiAlias = true
         textPaint.textSize = textSize
         textPaint.color = textColor
+        textPaint.textAlign = Paint.Align.CENTER
+        textRect = Rect()
     }
 
     fun setSlideDataSet(dataSet: MutableList<String>) {
@@ -91,6 +108,9 @@ class SlideBarView constructor(context: Context, attrs: AttributeSet) : View(con
 
         // 设置背景圆角矩形外边框范围
         viewBgRectF = RectF(-centerX, -centerY, centerX, centerY)
+
+        // 每个字母的高度
+        letterHeight = h / letterArray.size
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -118,6 +138,76 @@ class SlideBarView constructor(context: Context, attrs: AttributeSet) : View(con
         if (showBackground) {
             canvas.drawRoundRect(viewBgRectF, roundRadius, roundRadius, backgroundPaint)
         }
+
+        for (i in letterArray.indices) {
+            //字母变色
+            if (touchIndex == i) {
+                //让当前字母变色
+                textPaint.color = Color.parseColor("#00CB87")
+                textPaint.typeface = Typeface.DEFAULT_BOLD
+            } else {
+                //其他字母不变色
+                textPaint.color = textColor
+                textPaint.typeface = Typeface.defaultFromStyle(Typeface.NORMAL)
+            }
+
+            //绘制辅助线
+//            drawGuides(canvas, i)
+
+            //绘制文字
+            val letter = letterArray[i]
+
+            /**
+             *   |
+             * --|------------------------>
+             *   |   ---
+             *   |  |   |
+             *   |  | · | h
+             *   |  |   |
+             *   |  |---|
+             *   |  |   |
+             *   |  | · | h
+             *   |  |   |
+             *   |  |---|
+             *   |  |   |
+             *   |  | · | h
+             *   |  |   |
+             *   |   ---
+             *   ∨
+             *
+             *   每个字母的纵坐标 = View上边纵坐标+每个字母所在位置的纵坐标
+             *   每个字母所在位置的纵坐标 = (2 * i + 1) / 2 * 字母平均高度
+             * */
+
+            //每个文字左下角坐标
+            val textY = -centerY + (2 * i + 1) * 0.5 * letterHeight
+
+            //计算文字高度
+            textPaint.getTextBounds(letter, 0, letter.length, textRect)
+            val textHeight = textRect.height()
+
+            canvas.drawText(letter, 0f, textY.toFloat() + textHeight / 2, textPaint)
+        }
+    }
+
+    /**
+     * 辅助线
+     * */
+    private fun drawGuides(canvas: Canvas, i: Int) {
+        canvas.drawCircle(
+            0f,
+            (-centerY + (2 * i + 1) * 0.5 * letterHeight).toFloat(),
+            5f,
+            ancherPaint
+        )
+
+        canvas.drawLine(
+            -centerX,
+            -centerY + letterHeight * (i + 1).toFloat(),
+            centerX,
+            -centerY + letterHeight * (i + 1).toFloat(),
+            tickPaint
+        )
     }
 
     //侧边栏滑动事件
