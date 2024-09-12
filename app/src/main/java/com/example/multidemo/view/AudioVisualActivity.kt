2d1fbf602989aed71bea0bf45dcb468e6b7efabd
pengxianhong <290677893@qq.com>
2024-09-12 21:35:40 +0800

    添加音频数据捕捉

--- app/src/main/java/com/example/multidemo/view/AudioVisualActivity.kt
+++ app/src/main/java/com/example/multidemo/view/AudioVisualActivity.kt
@@ -1,17 +1,24 @@
 package com.example.multidemo.view
 
 import android.content.Intent
+import android.media.MediaPlayer
+import android.media.audiofx.Visualizer
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import com.example.multidemo.R
 import com.example.multidemo.databinding.ActivityAudioVisualBinding
 import com.example.multidemo.extensions.initImmersionBar
 import com.pengxh.kt.lite.base.KotlinBaseActivity
+import com.pengxh.kt.lite.extensions.realFilePath
+import java.io.FileInputStream
+import java.io.IOException
+
 
 class AudioVisualActivity : KotlinBaseActivity<ActivityAudioVisualBinding>() {
 
     private val kTag = "AudioVisualActivity"
+    private var mediaPlayer: MediaPlayer? = null
+    private var visualizer: Visualizer? = null
 
     override fun initEvent() {
         binding.selectAudioButton.setOnClickListener {
@@ -23,7 +30,39 @@ class AudioVisualActivity : KotlinBaseActivity<ActivityAudioVisualBinding>() {
 
         binding.playAudioButton.setOnClickListener {
             //播放音频文件
+            mediaPlayer = MediaPlayer()
+            try {
+                val fileDescriptor = FileInputStream(binding.audioFilePathView.text.toString()).fd
+                mediaPlayer?.setDataSource(fileDescriptor)
+                mediaPlayer?.prepare() // 准备媒体文件
+                mediaPlayer?.start() // 开始播放
 
+                //初始化可视化容器
+                visualizer = Visualizer(mediaPlayer!!.audioSessionId)
+                visualizer?.captureSize = Visualizer.getCaptureSizeRange()[1]
+                visualizer?.setDataCaptureListener(
+                    captureListener, Visualizer.getMaxCaptureRate() / 2, true, true
+                )
+                visualizer?.enabled = true
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private val captureListener = object : Visualizer.OnDataCaptureListener {
+        override fun onWaveFormDataCapture(
+            visualizer: Visualizer?, bytes: ByteArray?, samplingRate: Int
+        ) {
+            // 在这里处理波形数据
+            // bytes 数组包含了当前的波形数据
+            Log.d(kTag, "onWaveFormDataCapture: ${bytes.contentToString()}")
+        }
+
+        override fun onFftDataCapture(visualizer: Visualizer?, fft: ByteArray?, samplingRate: Int) {
+            // 注意：不是所有的 Visualizer 实现都会提供 FFT 数据
+            // 如果你的 Visualizer 提供了 FFT 数据，你可以在这里处理它
+            Log.d(kTag, "onFftDataCapture: ${fft.contentToString()}")
         }
     }
 
@@ -31,17 +70,10 @@ class AudioVisualActivity : KotlinBaseActivity<ActivityAudioVisualBinding>() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 10001 && resultCode == RESULT_OK && data != null) {
             val mp3Uri = data.data
-            Log.d(kTag, "onActivityResult: $mp3Uri")
             mp3Uri?.apply {
-                val filePathColumn = arrayOf(MediaStore.Audio.Media.DATA)
-                val cursor = contentResolver.query(this, filePathColumn, null, null, null)
-                if (cursor != null && cursor.moveToFirst()) {
-                    val columnIndex = cursor.getColumnIndex(filePathColumn[0])
-                    val mp3Path = cursor.getString(columnIndex)
-                    cursor.close()
-
-                    binding.audioFilePathView.setText(mp3Path.toString())
-                }
+                val absolutePath = this.realFilePath(this@AudioVisualActivity)
+                Log.d(kTag, "onActivityResult: $absolutePath")
+                binding.audioFilePathView.setText(absolutePath)
             }
         }
     }
@@ -61,5 +93,17 @@ class AudioVisualActivity : KotlinBaseActivity<ActivityAudioVisualBinding>() {
 
     override fun setupTopBarLayout() {
         binding.rootView.initImmersionBar(this, true, R.color.white)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (visualizer != null) {
+            visualizer?.release()
+            visualizer = null
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer?.release()
+            mediaPlayer = null
+        }
     }
 }
